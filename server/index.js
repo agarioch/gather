db2fabba077a3b03404784d5abe1ed62254d721f
agarioch <alistairgarioch@gmail.com>
2021-11-08 18:50:35 +0000

    feat(server): auth0 check Jwt
    Add express-oauth2-jwt-bearer and create a middleware function to check authentication for api
    endpoints

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const { auth } = require('express-oauth2-jwt-bearer');
 require('dotenv').config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const checkJwt = auth({
+  audience: 'http://localhost:5000',
+  issuerBaseURL: 'https://dev-a0zzgovu.us.auth0.com/'
+});
 
 app.use(cors());
 app.use(morgan('short'));
 
 app.get('/', (req, res) => res.send('<p>hello</p>'));
 
-app.listen(process.env.SERVER_PORT, () => console.log(`🚀 Server running on http://localhost:${process.env.SERVER_PORT}`));
+// test private route
+app.get('/private', checkJwt, function (req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
