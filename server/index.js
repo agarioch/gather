180c8cadb3d5de0f6f3748f9335dbffef64d3fc9
agarioch <alistairgarioch@gmail.com>
2021-11-08 18:30:03 +0000

    feat(server): add logger and cors middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('short'));
+
+app.get('/', (req, res) => res.send('<p>hello</p>'));
 
 app.listen(process.env.SERVER_PORT, () => console.log(`🚀 Server running on http://localhost:${process.env.SERVER_PORT}`));
