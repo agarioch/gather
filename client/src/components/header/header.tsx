b0a24f2ddd5c7e38dda629adb3638afa82c84878
agarioch <alistairgarioch@gmail.com>
2021-11-11 10:16:59 +0000

    fix(client-header): fix logo to use relative vs. absolute path

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -4,7 +4,7 @@ import './header.css';
 const Header: React.FC = ({ children }) => (
   <header className="header">
     <div className="header__logo">
-      <img src="gather-squirrel-primary.svg" alt="Gather mascot" height="30px" />
+      <img src="/gather-squirrel-primary.svg" alt="Gather mascot" height="30px" />
       <h1 className="header__logo--text">Gather</h1>
     </div>
     <div className="header--pull-right">{children}</div>
